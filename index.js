8e7786bc549a5ceb0b57ba10de0c650d67b807da
GeorgeAlTomeh <georgetoumeh134@gmail.com>
2025-09-03 03:40:44 +0300

    port binding

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const bot = new Telegraf(BOT_TOKEN);
